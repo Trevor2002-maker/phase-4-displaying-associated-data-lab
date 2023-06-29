6f642d3f6c9fcb62cf84e8acbbfb1b09d554412d
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-06-29 09:45:49 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
+# items_controller.rb
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.all.includes(:user)
+  
+      response = items.map do |item|
+        {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          price: item.price,
+          user: {
+            id: item.user.id,
+            username: item.user.username,
+            city: item.user.city
+          }
+        }
+      end
+  
+      render json: response
+    end
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
-end
+    def show
+      user = User.find(params[:id])
+      items = user.items
+  
+      response = {
+        id: user.id,
+        username: user.username,
+        city: user.city,
+        items: items.map do |item|
+          {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            price: item.price
+          }
+        end
+      }
+  
+      render json: response
+    end
+  end
